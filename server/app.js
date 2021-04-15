331de9bf41ef17de01e6f4bf4c47e8a6ac1ba72a
Hanna Boorom <boorom.hanna@gmail.com>
2021-04-15 13:38:18 -0500

    added schema file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+// naming this const graphqlHTTP is just a convention, not naming it the same as the package
+const graphqlHTTP = require('express-graphql')
+
 const app = express();
+
+app.use('/graphql',graphqlHTTP({
+
+}));
 
 app.listen(3000, () => {
     console.log('now listening for requests on port 3000')
