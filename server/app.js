f2329b30592e9f6a7dc11b5944c3202f92007baf
Hanna Boorom <boorom.hanna@gmail.com>
2021-04-15 13:59:17 -0500

    book schema tings, installed lodash

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 
 // naming this const graphqlHTTP is just a convention, not naming it the same as the package
-const graphqlHTTP = require('express-graphql')
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use('/graphql',graphqlHTTP({
-
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // same as writing schema: schema, just using ES6
+    schema,
+  })
+);
 
 app.listen(3000, () => {
-    console.log('now listening for requests on port 3000')
+  console.log("now listening for requests on port 3000");
 });
-
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+
+const books = [
+  { name: "Brave New World", genre: "Sci-Fi", id: "1" },
+  { name: "The Hunting Party", genre: "Mystery", id: "2" },
+  { name: "The Alchemist", genre: "Fantasy", id: "3" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -9,4 +18,24 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  // not wrapping the fields object in a function bc we don't need to worry about the order for the rootQuery
+  fields: {
+    // the name of the field will be what we use to make the query from the frontend
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new graphql.GraphQLSchema({
+  query: RootQuery,
 });
