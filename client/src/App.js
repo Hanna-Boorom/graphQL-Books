700a338c9cffa7bbc3c345cb16739a1baac7e25a
Hanna Boorom <boorom.hanna@gmail.com>
2021-04-27 14:06:19 -0500

    installed Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hanna's Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Hanna's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
